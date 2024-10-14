8fe003a1cd13edb12c050cad5305958e3509bbd0
Yong Yi Wong <yongyiwang.fullstack@gmail.com>
2024-10-14 00:25:49 -0700

    changed REST API path

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { TasksService } from './tasks/tasks.service';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const globalPrefix = configService.get('BACKEND_PREFIX');
+  app.setGlobalPrefix(globalPrefix);
 
   app.useGlobalPipes(
     new ValidationPipe({
